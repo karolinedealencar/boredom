00e1f9d2f90d750335a2dc366a8671f139b601e1
Karoline Alencar <karolinedalencar@gmail.com>
2020-04-30 09:49:00 -0300

    Updates

---
+++ precache-manifest.ae9206fb094332d01206a91e8bf09d3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89f9f5b04e9eb837e64ad49cb5bb7d1",
+    "url": "/boredom/index.html"
+  },
+  {
+    "revision": "9bb59fe016cbff09e4f1",
+    "url": "/boredom/static/css/main.d5f948f8.chunk.css"
+  },
+  {
+    "revision": "b1439788407657ce97bc",
+    "url": "/boredom/static/js/2.27a51b05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/boredom/static/js/2.27a51b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb59fe016cbff09e4f1",
+    "url": "/boredom/static/js/main.02adecd5.chunk.js"
+  },
+  {
+    "revision": "82fccb652f5df7349fe6",
+    "url": "/boredom/static/js/runtime-main.c4fbda45.js"
+  }
+]);
